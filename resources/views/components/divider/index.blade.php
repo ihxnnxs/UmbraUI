b927f87db681731459f86abca8d5d9afa1324fb8
ihxnnxs <crashermoney0@gmail.com>
2025-09-05 18:38:02 +0300

    fix vertical orientation

--- resources/views/components/divider/index.blade.php
+++ resources/views/components/divider/index.blade.php
@@ -6,16 +6,16 @@
 @if($hasContent)
     <div @class([
     'flex items-center',
-    'flex-col' => $isVertical,
+    'flex-col h-32' => $isVertical,
     'my-4' => !$isVertical,
     'mx-4' => $isVertical
 ])>
         <div @class(['flex-1 bg-neutral-300 dark:bg-neutral-700', 'h-0.5' => !$isVertical, 'w-0.5' => $isVertical])></div>
-        <div @class(['text-sm text-neutral-600 dark:text-neutral-400', 'px-4' => !$isVertical, 'py-3' => $isVertical])>
+        <div @class(['text-sm text-neutral-600 dark:text-neutral-400 whitespace-nowrap', 'px-4' => !$isVertical, 'py-3' => $isVertical])>
             {{ $slot }}
         </div>
         <div @class(['flex-1 bg-neutral-300 dark:bg-neutral-700', 'h-0.5' => !$isVertical, 'w-0.5' => $isVertical])></div>
     </div>
 @else
-    <hr {{ $attributes->except('orientation')->merge(['class' => $isVertical ? 'w-0.5 mx-4 border-l border-neutral-300 dark:border-neutral-700 inline-block' : 'h-0.5 my-4 border-0 bg-neutral-300 dark:bg-neutral-700']) }} />
+    <hr {{ $attributes->except('orientation')->merge(['class' => $isVertical ? 'w-0.5 h-16 mx-4 border-l border-neutral-300 dark:border-neutral-700 inline-block' : 'h-0.5 my-4 border-0 bg-neutral-300 dark:bg-neutral-700']) }} />
 @endif
